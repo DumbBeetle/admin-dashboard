a6faba07695e353e3525bca1ff025f450152c8c1
Ami <amidamri12@gmail.com>
2023-02-16 14:36:47 +0200

    start

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./Routes/general.js";
+import managementRoutes from "./Routes/management.js";
+import salesRoutes from "./Routes/sales.js";
+
+// Configuration
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(cors());
+
+// Routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// Mongoose Setup
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
