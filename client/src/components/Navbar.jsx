4c8240f6ceffd7be8d228e1eadc6a8f797417352
Amida <amidamri12@gmail.com>
2023-02-17 12:04:49 +0200

    start

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "components/FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import profileImage from "assets/profile.jpeg";
+import { AppBar, IconButton, Toolbar, useTheme } from "@mui/material";
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  return (
+    <AppBar sx={{ position: "static", background: "none", boxShadow: "none" }}>
+      <Toolbar sx={{ justifyContent: "space=between" }}>
+        {/*{leftSide}*/}
+        <FlexBetween>
+          <IconButton onClick={() => console.log("open/close sidebar")}>
+            <MenuIcon />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
