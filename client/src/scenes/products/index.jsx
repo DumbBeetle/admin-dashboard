1196520f39b8cd173691cba879e69eb9ffbe0c3c
Amida <amidamri12@gmail.com>
2023-03-01 21:32:36 +0200

    add Product page

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -1,18 +1,8 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Card,
-  CardActions,
-  CardContent,
-  Collapse,
-  Button,
-  Typography,
-  Rating,
-  useTheme,
-  useMediaQuery,
-} from "@mui/material";
+import React from "react";
+import { Box, useMediaQuery } from "@mui/material";
 import Header from "components/Header";
 import { useGetProductsQuery } from "state/api";
+import Product from "scenes/products/Product.jsx";
 
 const Products = () => {
   const { data, isLoading } = useGetProductsQuery();
@@ -21,7 +11,44 @@ const Products = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="Products" subtitle="See" />
-      {data || !isLoading ? <Box mt="20px" display="grid"></Box> : <></>}
+      {data || !isLoading ? (
+        <Box
+          mt="20px"
+          display="grid"
+          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+          justifyContent="space-between"
+          rowGap="20px"
+          columnGap="1.33%"
+          sx={{ "& > div": { gridColumn: isNonMobile ? undefined : "span 4" } }}
+        >
+          {data.map(
+            ({
+              _id,
+              name,
+              description,
+              price,
+              rating,
+              category,
+              supply,
+              stat,
+            }) => (
+              <Product
+                key={_id}
+                _id={_id}
+                name={name}
+                description={description}
+                price={price}
+                rating={rating}
+                category={category}
+                supply={supply}
+                stat={stat}
+              />
+            )
+          )}
+        </Box>
+      ) : (
+        <>Loading...</>
+      )}
     </Box>
   );
 };
